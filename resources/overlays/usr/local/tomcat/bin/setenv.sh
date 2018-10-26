a6adfa6e52547a368a2928ce3fac9cbb3d74fbce
Paoloc68 <paol.cesare.calvi@gmail.com>
2018-10-26 13:43:00 +0000

    Server changes

--- resources/overlays/usr/local/tomcat/bin/setenv.sh
+++ resources/overlays/usr/local/tomcat/bin/setenv.sh
@@ -1,2 +1,2 @@
-JAVA_OPTS="$JAVA_OPTS -Xmx1536M -XX:MaxPermSize=756M"
-JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled
+JAVA_OPTS="$JAVA_OPTS -Xms1024M -Xmx1536M"
+JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
